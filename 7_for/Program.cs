ee65b59685603554e4e8cbeb1cf90abd64a12c51
Flpp <flpp.bkm@gmail.com>
2020-08-16 16:22:47 -0300

    funções e for

--- 7_for/Program.cs
+++ 7_for/Program.cs
@@ -6,7 +6,28 @@ namespace _7_for
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Quantos números inteiros vc vai digitar?");
+
+            int quantidade = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+
+            for (int i = 1; i <= quantidade; i++)
+            {
+                if(i < quantidade)
+                {
+                    System.Console.WriteLine($"Digite o {i}º número:");
+                    int number = int.Parse(Console.ReadLine());
+                    soma = soma + number;
+                    System.Console.WriteLine($"A soma parcial é igual : {soma} ");
+                }
+                else{
+                    System.Console.WriteLine($"Digite o {i}º e último número:");
+                    int number = int.Parse(Console.ReadLine());
+                    soma = soma + number;
+                    System.Console.WriteLine($"A soma final é igual : {soma} ");
+                }
+            }
         }
     }
 }
