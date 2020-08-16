78813b09613941a4e7e7b89ec0bad307616dfa25
Flpp <flpp.bkm@gmail.com>
2020-08-16 00:50:36 -0300

    if else

--- 4_if_else/Program.cs
+++ 4_if_else/Program.cs
@@ -8,11 +8,19 @@ namespace _4_if_else
         {
             System.Console.WriteLine("Iniciando condicional");
 
-            if (Console.ReadLine().ToLower() == "bacon"){
-                System.Console.WriteLine("Hello Bacon");
+            string comida = Console.ReadLine().ToLower();
+
+            if (comida == "bacon"){
+                System.Console.WriteLine("Hello Bacon!");
+            }
+            else if (comida == "eggs"){
+                System.Console.WriteLine("Hello Eggs!");
+            }
+            else if (comida == "coffee"){
+                System.Console.WriteLine("Hello Coffee!");
             }
             else{
-                System.Console.WriteLine("Wait a minute... WHERE'S THE BACON????");
+                System.Console.WriteLine("Wait a minute... WHERE'S MY BREAKFEAST????");
             }
 
         }
