eb6e5d3ed8c35f5901b1f036a09d201a666c32e3
Flpp <flpp.bkm@gmail.com>
2020-08-16 16:00:55 -0300

    while

---
+++ 5_funcoes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _5_funcoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 3 números, por favor!");
+
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine($"1º número = {n1}");
+            System.Console.WriteLine($"2º número = {n2}");
+            System.Console.WriteLine($"3º número = {n3}");
+
+            int big_number = Maior(n1, n2, n3);
+            System.Console.WriteLine($"O maior número é : {big_number}");
+
+        }
+
+        static int Maior(int number1, int number2, int number3)
+        {
+            int number;
+            if (number1 > number2 && number1 > number3){
+                number = number1;
+            }
+            else if (number2 > number3){
+                number = number2;
+            }
+            else{
+                number = number3;
+            }
+            return number;
+        }
+
+
+    }
+}
---
+++ 6_while/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _6_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número, por favor");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 10){
+                while (number < 10){
+                    System.Console.WriteLine($"Number = {number}, ainda é menor que 10");
+                    number++;
+                }
+            System.Console.WriteLine($"Number = {number}, Fim do while");
+            }
+            else{
+                System.Console.WriteLine($"Number = {number}, não passara pelo while");
+            }
+        }
+    }
+}
