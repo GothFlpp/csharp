35846f72b91a54f53ab84b3920970a4a6692e1da
Flpp <flpp.bkm@gmail.com>
2020-08-16 00:41:16 -0300

    Bacon

--- 4_if_else/Program.cs
+++ 4_if_else/Program.cs
@@ -6,7 +6,15 @@ namespace _4_if_else
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Iniciando condicional");
+
+            if (Console.ReadLine().ToLower() == "bacon"){
+                System.Console.WriteLine("Hello Bacon");
+            }
+            else{
+                System.Console.WriteLine("Wait a minute... WHERE'S THE BACON????");
+            }
+
         }
     }
 }
